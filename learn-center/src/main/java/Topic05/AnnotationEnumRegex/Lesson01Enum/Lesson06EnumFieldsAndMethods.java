58a35b0a67759c9763803e62ffe5d8b04f18a353
farhadnooriii <f.noori.niaraki@outlook.com>
2022-08-14 22:20:52 +0430

    refactor

--- learn-center/src/main/java/Topic05/AnnotationEnumRegex/Lesson01Enum/Lesson06EnumFieldsAndMethods.java
+++ learn-center/src/main/java/Topic05/AnnotationEnumRegex/Lesson01Enum/Lesson06EnumFieldsAndMethods.java
@@ -3,29 +3,37 @@ package Topic05.AnnotationEnumRegex.Lesson01Enum;
 public class Lesson06EnumFieldsAndMethods {
 
     enum Directions {
-        EAST("E"),
-        WEST("W"),
-        NORTH("N"),
-        SOUTH("S");
+        EAST("E","east"),
+        WEST("W", "west"),
+        NORTH("N", "north"),
+        SOUTH("S", "south");
 
         // Important Note: Must have semicolon at
         // the end when there is a enum field or method
 
         private final String shortCode;
+        private final String shortValue;
 
-        Directions(String code) {
+        Directions(String code,String value) {
             this.shortCode = code;
+            this.shortValue = value;
         }
 
         public String getDirectionCode() {
             return this.shortCode;
         }
+
+        public String getDirectionValue() {
+            return this.shortValue;
+        }
     }
 
     public static void main(String[] args) {
-        Directions dir = Directions.SOUTH;
-        System.out.println(dir.getDirectionCode());
+        Directions dir1 = Directions.SOUTH;
+        System.out.println(dir1.getDirectionCode());
+        System.out.println(dir1.getDirectionValue());
         Directions dir2 = Directions.EAST;
         System.out.println(dir2.getDirectionCode());
+        System.out.println(dir2.getDirectionValue());
     }
 }
--- learn-center/src/main/java/Topic05/AnnotationEnumRegex/Lesson02Annotaion/Animal.java
+++ learn-center/src/main/java/Topic05/AnnotationEnumRegex/Lesson02Annotaion/Animal.java
@@ -1,6 +1,7 @@
 package Topic05.AnnotationEnumRegex.Lesson02Annotaion;
 
 public class Animal {
+
     void eatSomething(){
         System.out.println("eating something");
     }
